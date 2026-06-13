ed09622df661937bcd8eee55ef81ed9e74104d56
M H Jithin Muthappa <jithinmuthappa@gmail.com>
2026-06-13 20:31:46 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
