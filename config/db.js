f5ce1ad77e0552ed255b6a48ff3557b5732c323f
M H Jithin Muthappa <jithinmuthappa@gmail.com>
2026-06-13 12:35:06 +0530

    Fix SSL for Neon PostgreSQL

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL.includes('neon.tech')
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 pool.connect((err, client, release) => {
